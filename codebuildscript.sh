4489953b339daaa743553b56603a37b9d07841da
satyamchaurasiapersistent <102941840+satyamchaurasiapersistent@users.noreply.github.com>
2023-05-11 13:15:09 +0530

    Exit code script added

--- codebuildscript.sh
+++ codebuildscript.sh
@@ -2,7 +2,9 @@ EXIT_CODE=$(grep 'Finished with exit code:' cx-flow.log | tail -1 |sed 's/.*: //
 echo $EXIT_CODE
 if [ $EXIT_CODE != 0 ]
 then
+        echo 'Exit code is Non Zero so beaking pipeline.'
         exit 1
 else
+        echo 'Exit code is Zero so not beaking pipeline.'
         exit 0
 fi
